577b08297b95f350f7e405837158347c45501c7e
maggicco <maggicco83@hotmail.fr>
2021-12-30 20:51:17 +0100

    Added ProjectDataRepository method

--- app/src/main/java/com/cleanup/todoc/Repository/ProjectDataRepository.java
+++ app/src/main/java/com/cleanup/todoc/Repository/ProjectDataRepository.java
@@ -1,4 +1,25 @@
 package com.cleanup.todoc.Repository;
 
+import androidx.lifecycle.LiveData;
+
+import com.cleanup.todoc.Dao.ProjectDao;
+import com.cleanup.todoc.model.Project;
+
+import java.util.List;
+
 public class ProjectDataRepository {
+
+    private final ProjectDao projectDao;
+
+    public ProjectDataRepository(ProjectDao projectDao) {
+        this.projectDao = projectDao;
+    }
+
+    // --- Get Project ---
+    public LiveData<List<Project>> getAllProjects() {
+
+        return this.projectDao.getAllProjects();
+
+    };
+
 }
