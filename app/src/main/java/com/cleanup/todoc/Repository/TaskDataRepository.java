41f7bd402e7407e8a773b4c53831bf1a0116cb97
maggicco <maggicco83@hotmail.fr>
2021-12-30 21:01:03 +0100

    Added TaskDataRepository methods

--- app/src/main/java/com/cleanup/todoc/Repository/TaskDataRepository.java
+++ app/src/main/java/com/cleanup/todoc/Repository/TaskDataRepository.java
@@ -1,4 +1,69 @@
 package com.cleanup.todoc.Repository;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.cleanup.todoc.Dao.TaskDao;
+import com.cleanup.todoc.model.Task;
+
+import java.util.List;
+
 public class TaskDataRepository {
+
+    private final TaskDao taskDao;
+
+    public TaskDataRepository(TaskDao taskDao) {
+        this.taskDao = taskDao;
+    }
+
+    //  --- Get List of Tasks ---
+    public LiveData<List<Task>> getTaskList() {
+
+        return this.taskDao.getTaskList();
+
+    }
+
+    //  --- Get List of Tasks ---
+    public LiveData<List<Task>> getAzTaskList() {
+
+        return this.taskDao.getAzTaskList();
+
+    }
+
+    //  --- Get List of Tasks ---
+    public LiveData<List<Task>> getZaTaskList() {
+
+        return this.taskDao.getZaTaskList();
+
+    }
+
+    //  --- Get List of Tasks ---
+    public LiveData<List<Task>> getOldestTaskList() {
+
+        return this.taskDao.getOldestTaskList();
+
+    }
+
+    //  --- Get List of Tasks ---
+    public LiveData<List<Task>> getYoungestTaskList() {
+
+        return this.taskDao.getYoungestTaskList();
+
+    }
+
+    //  --- Create a Task ---
+    public void insertTask(Task task) {
+
+        taskDao.insertTask(task);
+
+    }
+
+    //  ---Delete a Task---
+    public void deleteTask(Task task) {
+
+        taskDao.deleteTask(task);
+
+    }
 }
